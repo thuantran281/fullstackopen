52a6c2c688876dd71817e03424e4f00600bcafde
Thuan Tran <tranthuan12002@gmail.com>
2022-11-08 16:37:26 +0200

    ex1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -6,18 +6,20 @@ const Header = (props) => {
   );
 };
 
+const Part = (props) => {
+  return (
+    <>
+      <p>{props.part} {props.exercise}</p>
+    </>
+  )
+}
+
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercise1}
-      </p>
-      <p>
-        {props.part2} {props.exercise2}
-      </p>
-      <p>
-        {props.part3} {props.exercise3}
-      </p>
+      <Part part={props.part1} exercise={props.exercise1} />
+      <Part part={props.part2} exercise={props.exercise2} />
+      <Part part={props.part3} exercise={props.exercise3} />
     </>
   );
 };
