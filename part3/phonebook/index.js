96de01b06af8e7366bda5052758c51d67eded3d0
Thuan Tran <tranthuan12002@gmail.com>
2023-09-06 20:04:56 +0300

    ex3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -37,6 +37,17 @@ app.get("/info", (request, response) => {
   );
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
 const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
