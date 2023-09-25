a8761135441003c33e2c5afe8c06ae712479fe15
Thuan Tran <tranthuan12002@gmail.com>
2023-09-25 22:12:27 +0300

    ex3.7 & 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+app.use(express.json());
+app.use(morgan("tiny"));
+
+morgan.token("postInfo", function (req) {
+  return req.body ? JSON.stringify(req.body) : "";
+});
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :postInfo"
+  )
+);
 
 app.use(express.json());
 
