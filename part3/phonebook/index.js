0ef01ba38a3e92f33673b4cba93b83bb82f2ec68
Thuan Tran <tranthuan12002@gmail.com>
2023-10-03 23:16:28 +0300

    ex3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.static("build"));
 
 morgan.token("postInfo", function (req) {
   return req.body ? JSON.stringify(req.body) : "";
@@ -13,8 +16,6 @@ app.use(
     ":method :url :status :res[content-length] - :response-time ms :postInfo"
   )
 );
-
-app.use(express.json());
 
 let notes = [
   {
@@ -99,7 +100,7 @@ app.post("/api/persons", (request, response) => {
   response.json(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
